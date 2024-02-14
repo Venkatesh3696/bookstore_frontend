cb88b222fddf86e730abe7fad1812e0ca74e2317
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-02-15 02:41:02 +0530

    completed frontend functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/books/create" element={<CreateBooks />} />
         <Route path="/books/details/:id" element={<ShowBook />} />
         <Route path="/books/edit/:id" element={<EditBook />} />
-        <Route path="/books/deleet/:id" element={<DeleteBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
       </Routes>
     </>
   );
--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DeleteBook = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log("frontend", id);
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error happened. please check console");
+        console.log("here =>  ", error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl 2-[600px] p-8 mx-auto">
+        <h3>Are you sure tou want to delete this book?</h3>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
